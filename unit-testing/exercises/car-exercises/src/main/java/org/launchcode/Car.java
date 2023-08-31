76535dc4de722b4ad3bcea469befa0165fd677b1
MuntoniFrancesca <francesca.muntoni@live.com>
2023-08-31 14:59:03 -0500

    unit testing finished exercises

--- unit-testing/exercises/car-exercises/src/main/java/org/launchcode/Car.java
+++ unit-testing/exercises/car-exercises/src/main/java/org/launchcode/Car.java
@@ -16,7 +16,9 @@ public class Car {
         this.gasTankLevel = gasTankSize;
         this.milesPerGallon = milesPerGallon;
     }
-
+public void addGas(double gas) {
+        this.setGasTankLevel(gas + this.getGasTankLevel());
+}
     public String getMake() {
         return make;
     }
@@ -46,6 +48,9 @@ public class Car {
     }
 
     public void setGasTankLevel(double gasTankLevel) {
+        if (gasTankLevel > this.getGasTankSize()) {
+            throw new IllegalArgumentException("Can't exceed tank size");
+        }
         this.gasTankLevel = gasTankLevel;
     }
 
